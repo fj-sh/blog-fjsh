fba5482626932431d344d68904bec16b4a93504f
fj-sh <menspocky322@gmail.com>
2020-12-19 18:39:56 +0900

    Prismjsのテーマを変更した。Rubyの配列について追記した。

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,4 +2,5 @@
 
 import "katex/dist/katex.min.css"
 require('./src/assets/scss/init.scss');
-require('./static/css/prismjs/theme.min.css');
+// require('./static/css/prismjs/theme.min.css');
+require('./static/css/prismjs/prism-xonokai.css');
